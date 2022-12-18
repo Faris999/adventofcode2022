51e3c320562f15a73140cee6756aba9128b4578e
Faris Hafizhan <farishafiz999@gmail.com>
2022-12-18 07:48:55 +0700

    update day 1 haskell

--- day01/day1.hs
+++ day01/day1.hs
@@ -2,24 +2,21 @@ import Data.List
 
 process :: [String] -> [[String]]
 process [] = []
-process a = (takeWhile (/= "") a) : (process (case dropWhile (/= "") a of
+process a = takeWhile (/= "") a : process (case dropWhile (/= "") a of
                                         [] -> []
-                                        (x:xs) -> xs))
+                                        (x:xs) -> xs)
 
 process' :: [[String]] -> [Int]
-process' x = map (sum . map read) x
+process' = map (sum . map read)
 
--- process'' :: [String] -> [Int]
--- process'' a = (sum $ map read (takeWhile (/= "") a)) : (process'' (case dropWhile (/= "") a of
-                                        -- [] -> []
-                                        -- (x:xs) -> xs))
+process'' :: [String] -> [Int]
+process'' = process' . process
 
 main :: IO ()
 main = do
     input <- readFile "input.txt"
     let fileLines = lines input
-    let x = process fileLines
-    let y = process' x
-    let calories = reverse $ sort $ y
-    putStrLn $ show $ head calories
-    putStrLn $ show $ sum $ take 3 calories
+    let y = process'' fileLines
+    let calories = reverse $ sort y
+    print $ head calories
+    print $ sum $ take 3 calories
